d747c08f5da36fafbfc4045beb014c7aaf8f54e6
slammedd <kyleajames102@gmail.com>
2021-02-11 15:21:07 +0000

    Created dash refil powerup.

---
+++ Assets/Scripts/DashRefil.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashRefil : MonoBehaviour
+{
+    public PlayerControllerRigidbody playerController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("powerup");
+            playerController.hasDashed = false;
+            Destroy(this.gameObject);
+        }
+    }
+}
