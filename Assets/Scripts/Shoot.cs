af8aa1384ae17ea3eace4d6179748753b5e5a518
slammedd <kyleajames102@gmail.com>
2021-02-17 14:43:18 +0000

    Completed shooting.

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,14 +6,18 @@ public class Shoot : MonoBehaviour
 {
     public float damage;
     public float range;
+    public float impactForce;
+    public float fireRate;
     public Camera playerCamera;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
+    private float fireCooldown = 0;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButton(0) && Time.time >= fireCooldown)
         {
+            fireCooldown = Time.time + 1 / fireRate;
             Shot();
         }
     }
@@ -24,16 +28,22 @@ public class Shoot : MonoBehaviour
         RaycastHit hit;
 
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, range))
-        {            
+        {
             Enemy enemy = hit.transform.GetComponent<Enemy>();
-            
-            if(hit.transform.tag == "Enemy")
+
+            if (hit.transform.tag == "Enemy")
             {
                 Debug.Log("Hit");
                 enemy.Damaged(damage);
             }
-            
-            Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+
+            GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impact, 2);
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
         }
 
     }
