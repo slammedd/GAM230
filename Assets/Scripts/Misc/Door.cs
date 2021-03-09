8a93287e8d0de8f588c1ae5ea5227e484c2f025d
slammedd <kyleajames102@gmail.com>
2021-03-09 10:58:02 +0000

    Increased coyote time and added doors.

---
+++ Assets/Scripts/Misc/Door.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Animator doorAnimator;
+   
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnimator.SetBool("Trigger", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorAnimator.SetBool("Trigger", false);
+        }
+    }
+}
