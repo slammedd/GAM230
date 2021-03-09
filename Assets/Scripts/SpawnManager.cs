edc584827a4f173f649e3e73d6b2ea603b59e956
slammedd <kyleajames102@gmail.com>
2021-03-09 01:02:30 +0000

    Started creating spawn manager.

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public LayerMask acid;
+    public Transform[] spawnPoints;
+
+    private float checkRadius = 0.2f;
+    private bool kill;
+    private Transform acidCheck;
+    private Animator screenWipeAnimator;
+    private int spawnCounter = 0;
+
+    private void Start()
+    {
+        acidCheck = GameObject.Find("Player").GetComponent<PlayerControllerRigidbody>().groundCheck;
+        screenWipeAnimator = GameObject.Find("Screen Wipe").GetComponent<Animator>();
+
+        GameObject.Find("Player").transform.position = spawnPoints[spawnCounter].position;
+
+        if(GameObject.Find("Player").transform.position == spawnPoints[0].position)
+        {
+            screenWipeAnimator.SetTrigger("Trigger");
+        }
+    }
+
+    private void Update()
+    {
+        Kill();
+    }
+
+    void Kill()
+    {
+        kill = Physics.CheckSphere(acidCheck.position, checkRadius, acid);
+
+        if (kill)
+        {
+            GameObject.Find("Player").transform.position = spawnPoints[spawnCounter].position;
+        }
+    }
+
+}
