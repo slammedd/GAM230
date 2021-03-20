3278678f93014e426deda5ff5a921df3fa19a38e
KJ232728 <kyleajames102@gmail.com>
2021-03-20 17:00:06 +0000

    Added animated lava floors.

---
+++ Assets/Scripts/Misc/TextureAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureAnimator : MonoBehaviour
+{
+    public float xSpeed;
+    public float ySpeed;
+
+    private float currentX;
+    private float currentY;
+
+    private void Start()
+    {
+        currentX = GetComponent<Renderer>().material.mainTextureOffset.x;
+        currentY = GetComponent<Renderer>().material.mainTextureOffset.y;
+    }
+
+    private void Update()
+    {
+        currentX += Time.deltaTime * xSpeed;
+        currentY += Time.deltaTime * ySpeed;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(currentX, currentY));
+    }
+}
