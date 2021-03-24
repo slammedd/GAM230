73cae0162a037ab3cafcc729dfc1a9810b96b481
Kyle James <kyleajames102@gmail.com>
2021-03-24 13:07:16 +0000

    Removed physmat checker from unnecessary objects.

---
+++ Assets/Scripts/Player/HealthPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    public PlayerControllerRigidbody playerController;
+    public int healthToAdd;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerController.health += healthToAdd;
+            Destroy(gameObject);
+        }
+    }
+}
