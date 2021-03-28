d1ca5205bff01251b65bbea435ec3b5c489bcc8f
Kyle James <kyleajames102@gmail.com>
2021-03-28 20:44:45 +0100

    Added door sounds.

--- Assets/Scripts/Misc/Door.cs
+++ Assets/Scripts/Misc/Door.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     public Animator doorAnimator;
+    public AudioSource source;
+    public AudioClip doorOpenSound;
+    public AudioClip doorCloseSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            source.PlayOneShot(doorOpenSound);
             doorAnimator.SetBool("Trigger", true);
         }
     }
@@ -18,6 +22,7 @@ public class Door : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
+            source.PlayOneShot(doorCloseSound);
             doorAnimator.SetBool("Trigger", false);            
         }
     }
