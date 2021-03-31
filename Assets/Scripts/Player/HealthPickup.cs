ea6b56911b9909c8374795f160a1fd52b4134c9a
Kyle James <kyleajames102@gmail.com>
2021-03-31 18:08:01 +0100

    Added health pickup sound.

--- Assets/Scripts/Player/HealthPickup.cs
+++ Assets/Scripts/Player/HealthPickup.cs
@@ -6,12 +6,21 @@ public class HealthPickup : MonoBehaviour
 {
     public PlayerControllerRigidbody playerController;
     public int healthToAdd;
+    public AudioClip healthPickupSound;
+
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = GameObject.Find("Head").GetComponent<AudioSource>();   
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerController.health += healthToAdd;
+            source.PlayOneShot(healthPickupSound);
             Destroy(gameObject);
         }
     }
