e51464bea3dc109f25274d39cb13c81507f50333
Kyle James <kyleajames102@gmail.com>
2021-04-04 23:13:25 +0100

    Created menu animations.

--- Assets/Scripts/Misc/MainMenu.cs
+++ Assets/Scripts/Misc/MainMenu.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Animator noteBackground;
+    public Animator[] noteTextAnimators;
     public Animator[] buttonAnimators;
-    public GameObject selectionArrows;
+    public AudioSource source;
+    public AudioClip UISound;
+    public GameObject[] mainMenuObjects;
 
     private void Start()
     {
-        StartCoroutine(MenuAnimations());
+        StartCoroutine(NoteAnimations());
     }
 
     IEnumerator MenuAnimations()
@@ -19,10 +24,37 @@ public class MainMenu : MonoBehaviour
         {
             anim.SetBool("Trigger", true);
         }
-        yield return new WaitForSeconds(2);
-        selectionArrows.SetActive(true);
-
     }
 
+    IEnumerator NoteAnimations()
+    {
+        yield return new WaitForSeconds(3);
+        noteBackground.SetBool("Trigger", true);
+        foreach (Animator anim in noteTextAnimators)
+        {
+            anim.SetBool("Trigger", true);
+        }
+
+        yield return new WaitForSeconds(1);
+
+        foreach(GameObject gO in mainMenuObjects)
+        {
+            gO.SetActive(true);
+        }
+
+        StartCoroutine(MenuAnimations());
+    }
+
+    public void StartGame()
+    {
+        source.PlayOneShot(UISound);
+        SceneManager.LoadScene("Levels");
+    }
+
+    public void QuitGame()
+    {
+        source.PlayOneShot(UISound);
+        Application.Quit();
+    }
 
 }
