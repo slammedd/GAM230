ee15a9f719b3f79d1b47727284c986494d3114c8
Kyle James <kyleajames102@gmail.com>
2021-04-04 20:35:15 +0100

    Added main menu animations.

---
+++ Assets/Scripts/Misc/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public Animator[] buttonAnimators;
+    public GameObject selectionArrows;
+
+    private void Start()
+    {
+        StartCoroutine(MenuAnimations());
+    }
+
+    IEnumerator MenuAnimations()
+    {
+        yield return new WaitForSeconds(2);
+        foreach (Animator anim in buttonAnimators)
+        {
+            anim.SetBool("Trigger", true);
+        }
+        yield return new WaitForSeconds(2);
+        selectionArrows.SetActive(true);
+
+    }
+
+
+}
