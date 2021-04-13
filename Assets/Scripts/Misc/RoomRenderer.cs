4ecf931c8b3f7c35385705589b2ec23660a02d1d
Kyle James <kyleajames102@gmail.com>
2021-04-13 14:43:49 +0100

    Removed mesh walls and added occlusion culling.

---
+++ Assets/Scripts/Misc/RoomRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomRenderer : MonoBehaviour
+{
+    public GameObject previousRoom;
+    public GameObject nextRoom;
+    public GameObject doorFacade;
+    public GameObject[] previousRoomWalls;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            previousRoom.SetActive(false);
+        }
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1);
+
+            nextRoom.SetActive(true);
+            doorFacade.SetActive(true);
+
+
+    }
+}
